57eb2b972b772eb15b4cee668a69639097058bda
Godwin <godwinchukks@gmail.com>
2024-12-10 15:22:15 -0800

    updated

--- stop_container.sh
+++ stop_container.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-set -e
 
-echo "done"
+# Get the container ID or name of the running container (change "your-image-name" to your Docker image name)
+CONTAINER_ID=$(docker ps -q --filter "ancestor=your-image-name")
 
-#sudo su - root
-# Stop the running container (if any)
-#docker stop $(docker ps -a -q)
-#docker rm $(docker ps -a -q)
+if [ -n "$CONTAINER_ID" ]; then
+  # Stop the container
+  docker stop "$CONTAINER_ID"
+  echo "Stopped container: $CONTAINER_ID"
+else
+  echo "No running container found for the image 'your-image-name'."
+fi
