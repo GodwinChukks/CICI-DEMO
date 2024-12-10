9d610c9a41f34f503792f5dcd659fef6e92f4d45
Godwin <godwinchukks@gmail.com>
2024-12-10 15:12:54 -0800

    updated

--- start_container.sh
+++ start_container.sh
@@ -4,7 +4,7 @@ set -e
 sudo su - root
 
 # Pull the Docker image from Docker Hub
-docker pull godwinchukks/codepipeline-demo
+docker pull godwinchukks/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 godwinchukks/codepipeline-demo
+docker run -d -p 5000:5000 godwinchukks/simple-python-flask-app
