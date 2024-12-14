1283744be8de2c2bab572711394ccf7aca1c83eb
GodwinChukks <godwinchukks@gmail.com>
2024-12-14 05:02:05 -0800

    Updated stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,8 +1,22 @@
-#!/bin/bash
-set -e
+##!/bin/bash
+#set -e
 
 #sudo su - root
 # Stop the running container (if any)
 #docker stop $(docker ps -a -q)
 #docker rm $(docker ps -a -q)
-echo "done"
+#echo "done"
+
+#!/bin/bash
+# stop_container script
+set -e
+
+# Stop and remove any running container using port 5000
+RUNNING_CONTAINER=$(docker ps -q --filter "ancestor=godwinchukks/simple-python-flask-app" --filter "status=running")
+if [ -n "$RUNNING_CONTAINER" ]; then
+    echo "Stopping running container..."
+    docker stop "$RUNNING_CONTAINER"
+    docker rm "$RUNNING_CONTAINER"
+else
+    echo "No running container found."
+fi
